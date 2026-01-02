cedb2280ce0357d07ce2aa5dbd9488ff3bd305e2
royalmillion <youremail@example.com>
2026-01-02 15:09:24 +0700

    update config.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`API SFB listening on http://localhost:${PORT}`);
